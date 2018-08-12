b85050359e4a66f4d3ad4bdab33cd0125d44608a
xiaokai <1106265613@qq.com>
2018-08-12 23:34:14 +0800

    修改

--- app01/views.py
+++ app01/views.py
@@ -25,4 +25,4 @@ def login(request):
 # url(r'^edit/(?P<a1>\w+)/(?P<a2>\w+)/',views.edit) 与views里对应参数对应，
 def edit(request,a1,a2): # 如果参数设置为*args,**kargs时，参数类型要保持一致，即上面两种形式
     print(a1,a2)
-    return HttpResponse('.....')
+    return HttpResponse('.....ok')
